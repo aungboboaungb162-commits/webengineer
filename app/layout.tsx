466d548a2de36f4d740f2579fe81c5bc5d7e7efe
aungboboaungb162-commits <aungbobo@macbook-air.local>
2026-05-05 05:20:07 +0630

    feat: update layout, config, and hero component

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <Navbar />
 
         {/* pt-40 ကို ဖယ်လိုက်ပါပြီ၊ ဒါမှ photo က header အောက်ထဲ တိုးဝင်သွားမှာပါ */}
-        <main className="min-h-screen pt-28">
+        <main className="min-h-screen pt-24">
           {children}
         </main>
 
